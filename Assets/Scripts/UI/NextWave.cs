fcc4e6232af60ccb6e885741e4f4a4fd54bf013e
kzzxc <toropovnik1802@gmail.com>
2022-11-10 18:03:59 +0300

    Init Project

---
+++ Assets/Scripts/UI/NextWave.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextWave : MonoBehaviour
+{
+    [SerializeField] private Spawner _spawner;
+    [SerializeField] private Button _nexWaveButton;
+
+    private void OnEnable()
+    {
+        _spawner.AllEnemySpawned += OnAllEnemySpawned;
+        _nexWaveButton.onClick.AddListener(OnNextWaveButtonClick);
+    }
+
+
+    private void OnDisable()
+    {
+        _spawner.AllEnemySpawned -= OnAllEnemySpawned;
+        _nexWaveButton.onClick.RemoveListener(OnNextWaveButtonClick);
+    }
+
+    public void OnAllEnemySpawned()
+    {
+        _nexWaveButton.gameObject.SetActive(true);
+    }
+
+    public void OnNextWaveButtonClick()
+    {
+        _spawner.NextWave();
+        _nexWaveButton.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Weapon/Shotgun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Shotgun : Weapon
+{
+    private float _accuracy;
+    private int _bulletsCount;
+
+    public override void Shoot(Transform shootPoint)
+    {
+        _accuracy = 0.3f;
+        _bulletsCount = 5;
+
+        for (int i = 0; i < _bulletsCount; i++)
+        {
+            float accuracyX = Random.Range( - _accuracy, _accuracy);
+            float accuracyY = Random.Range( - _accuracy, _accuracy);
+
+            Instantiate(Bullet, shootPoint.position + new Vector3(accuracyX, accuracyY, 0), Quaternion.identity);
+        }
+    }
+}
